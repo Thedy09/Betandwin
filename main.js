3860ef0797e0865deb92f8c65ad510ddcf10042a
Thedy09 <64496227+Thedy09@users.noreply.github.com>
2024-08-08 10:20:24 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// js/main.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Vous pouvez ajouter ici le code JavaScript commun à toutes les pages
+    console.log('THD09 Affiliates website loaded');
+});
